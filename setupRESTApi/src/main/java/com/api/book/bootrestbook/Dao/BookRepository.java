5a5965e853def11002224bb54aa13a080e4223ab
Nitesh Kumar <145526263+niteshkumar712@users.noreply.github.com>
2024-11-27 02:31:21 +0530

    Update BookRepository.java

--- setupRESTApi/src/main/java/com/api/book/bootrestbook/Dao/BookRepository.java
+++ setupRESTApi/src/main/java/com/api/book/bootrestbook/Dao/BookRepository.java
@@ -4,8 +4,8 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.api.book.bootrestbook.entities.Book;
 
-public interface BookRepository  extends CrudRepository<Book,Integer>{
-	public Book findById(int id);
+public interface BookRepository  extends CrudRepository<USER,Integer>{
+	public USER findById(int id);
 	
 	
 
